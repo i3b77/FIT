ee6e95a49187aab87fe90bfc340e511157cd1b9d
i3b77 <abodomar1212@gmail.com>
2023-09-25 22:04:48 +0300

    config

---
+++ config.py
@@ -0,0 +1,3 @@
+bind = '0.0.0.0:8000'  # The address and port to bind to
+workers = 4  # The number of worker processes
+timeout = 500  # The maximum time (in seconds) a worker can spend handling a request
